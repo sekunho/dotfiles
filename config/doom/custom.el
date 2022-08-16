510d46fed9f5f6c87ec0e8ce4e3ddc7ae8769e74
sekun <sekun@duck.com>
2022-08-16 21:52:13 +0800

    Update doom emacs config
           - Change theme because I got bored
           - Add org agenda path
           - Add `+pretty` in `org` package

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(haskell-compile-cabal-build-command "cabal build")
- '(haskell-compiler-type 'cabal))
+ '(haskell-compiler-type 'cabal)
+ '(org-agenda-files '("/home/noodle/Documents/org/agenda.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
