15deb39710a3c07386c7bf44e86821bac112f320
sekun <sekun@duck.com>
2023-04-17 01:05:12 +0800

    chore(mew): Remove public key

--- hosts/mew/configuration.nix
+++ hosts/mew/configuration.nix
@@ -49,7 +49,6 @@
   };
 
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnYwiKU31DCJCG9mAFJ72AhBVb/jfMVJm9UODTsPvVmtdsApnEnYanUboH1mM+z0W0XCEUWSHlRzqkFoTj2fOejalsBwALjKP8Bx+18SUIn5uEoy9FI9sjs/6vHx8Xt32fUhNU3r/inttFemRhpwodWooK537FbXqypt3dOcDbHr8anNO5xvdB+oscbPjHRJnp9j9iVsag31mynnSQe0yyIYooNTe77+0ZsxbgBtrooukEpLyOpdhL4iP7oWsXdb4xFM2xDlhD4MEdjblnom5ZmKKPssSuBV0HYnlOGmqjvXUj0xn+BlvbQiiTcTK14/KgldB+T5gc4R22s6VodTV0I6rTvdosudOTT4hHQecx9U2xN4xki27ygkGRcTJmrghShvE+lzU4ad4lotcKWv9AUGJopKURf8jtoF6AgDVYaQ42NhpVMwf4VW0Md/wWDDWXYh0N/4kRsCZEbRclDksLOuq3TbHP++wsoLfk5YTM0tu/+EDR/Cqdr0uXgaVYAyM= sekun@ichi"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINI269n68/pDDfMjkPaWeRUldzr1I/dWfUZl7sZPktwCAAAABHNzaDo= software@sekun.net"
   ];
 
